e55e95f1a6e2073d0400f83a36bd3bc1c26bb848
steven.zhang <steven.zhang@oriente.com>
2018-08-17 19:10:13 +0800

    add java doc

--- src/main/java/calculator/rpn/Interpreter.java
+++ src/main/java/calculator/rpn/Interpreter.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Iterator;
 
+/**
+ * Token interpreter ,parse token and operate numbers on token stack
+ */
 @Slf4j
 public class Interpreter implements TokenVisitor {
 
